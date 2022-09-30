27f477ed5430bc4c026bb16900ff5411686b360b
baki <zuo9527@icloud.com>
2022-09-30 18:55:42 +0800

    Outlet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+const Shop = () => {
+  return <>I am the shop page</>;
+};
 
 function App() {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}></Route>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="shop" element={<Shop />}></Route>
+      </Route>
     </Routes>
   );
 }
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 import Categories from "../../components/categories/categories.component";
 
 const Home = () => {
   return (
     <div>
       <Categories />
+      <Outlet />
     </div>
   );
 };
