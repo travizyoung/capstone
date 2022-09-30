ddd8f241e5a4dd53697a008e93a1c06635413de6
baki <zuo9527@icloud.com>
2022-09-30 19:16:58 +0800

    Style Navigation And Logo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
 
 const Shop = () => {
   return <>I am the shop page</>;
