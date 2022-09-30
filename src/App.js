0922781ec5c637a45e0aaf570f416890d2c0485f
baki <zuo9527@icloud.com>
2022-09-30 17:39:24 +0800

    Setting Up Our Homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Categories from "./components/categories/categories.component";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
 
 function App() {
   return (
-    <div>
-      <Categories />
-    </div>
+    <Routes>
+      <Route path="/home" element={<Home />}></Route>
+    </Routes>
   );
 }
 
