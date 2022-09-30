d728d819aa7fd2ce1c178e7ef4ec48e7924fa185
baki <zuo9527@icloud.com>
2022-09-30 21:56:08 +0800

    Only use Popup

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,20 +1,8 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
-
 import {
-  auth,
   signInWithGooglePopup,
   signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.util";
-
-const afterRedirectLogin = async () => {
-  const response = await getRedirectResult(auth);
-  if (response) {
-    const userDocRef = await createUserDocumentFromAuth(response.user);
-    console.log(userDocRef);
-  }
-};
 
 const SignIn = () => {
   const logGoogleUser = async () => {
@@ -22,17 +10,10 @@ const SignIn = () => {
     const userDocRef = await createUserDocumentFromAuth(user);
   };
 
-  useEffect(() => {
-    afterRedirectLogin();
-  }, []);
-
   return (
     <div>
       <h1>Sign in page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with Google Redirect
-      </button>
     </div>
   );
 };
